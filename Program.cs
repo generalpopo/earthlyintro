a3b284952c7d0f1a47fcbb9619ac7ccaec6bdcc2
Comfort Mokoena <mokoenacd@gmail.com>
2021-08-28 10:03:26 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //name = "gjgh"
+            string name = "Sheila";
+
+            for(int i = 0; i<= 10; i += 2){
+                Console.WriteLine(i);
+            }
+
+            //print('hello world')
+           // Console.WriteLine(name);
+        }
+    }
+}
